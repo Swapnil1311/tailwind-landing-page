1b082c9cb3368c11902ec84dbf0a004ecfde3385
Swapnil1311 <thakore.swapnil@gmail.com>
2024-12-10 16:56:45 +0530

    Update to new code

--- src/components/FeatureLine.jsx
+++ src/components/FeatureLine.jsx
@@ -19,19 +19,21 @@ export const FeatureLine = () => {
   }, []);
 
   return (
-    <div className="max-w-7xl mt-12 flex items-center overflow-hidden rounded-xl border border-solid px-6 py-8">
-      <section className="flex items-center" ref={line4Ref}>
-        {featureList.map((featureList, index) => (
-          <div key={index} className="flex">
-            <h2 className="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">
-              {featureList.title}
-            </h2>
-            <div className="my-0 mx-2 whitespace-nowrap font-display text-4xl font-semibold leading-7 text-gray-800">
-              <LuDot />
+    <div className="max-w-7xl mt-12 mx-auto px-6 lg:px-8">
+      <div className=" flex items-center overflow-hidden rounded-xl border border-solid px-6 py-8">
+        <section className="flex items-center" ref={line4Ref}>
+          {featureList.map((featureList, index) => (
+            <div key={index} className="flex">
+              <h2 className="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">
+                {featureList.title}
+              </h2>
+              <div className="my-0 mx-2 whitespace-nowrap font-display text-4xl font-semibold leading-7 text-gray-800">
+                <LuDot />
+              </div>
             </div>
-          </div>
-        ))}
-      </section>
+          ))}
+        </section>
+      </div>
     </div>
   );
 };
